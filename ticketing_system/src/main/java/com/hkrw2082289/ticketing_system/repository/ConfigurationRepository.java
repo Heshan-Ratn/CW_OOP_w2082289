5f22581b8e9d7cf2063a72b34d928ea5fd3bfaa6
Heshan Ratnaweera <heshan.20222094@iit.ac.lk>
2024-12-12 08:59:19 +0530

    Fix: removed draft versions of the CLI and GUI and Backend appications.

--- ticketing_system/src/main/java/com/hkrw2082289/ticketing_system/repository/ConfigurationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hkrw2082289.ticketing_system.repository;
-
-import com.hkrw2082289.ticketing_system.model.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigurationRepository extends JpaRepository<Configuration, Integer> {
-}
